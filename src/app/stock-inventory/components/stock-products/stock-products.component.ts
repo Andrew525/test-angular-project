e5979a8ace5eab81f28c3cf6e86e33acc0f86894
Andy <apodaiko@greenlightportal.com>
2019-10-06 19:58:11 +0300

    34 FormGroup Collection with FormArray

--- src/app/stock-inventory/components/stock-products/stock-products.component.ts
+++ src/app/stock-inventory/components/stock-products/stock-products.component.ts
@@ -1,11 +1,27 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import {Component, Input, OnInit} from '@angular/core';
+import {AbstractControl, FormArray, FormGroup} from '@angular/forms';
 
 @Component({
     selector: 'app-stock-products',
     template: `
         <div class="stock-product" [formGroup]="parent">
-
+            <div formArrayName="stock">
+                <div *ngFor="let item of stocks; let i = index;">
+                    <div class="stock-product__content" [formGroupName]="i">
+                        <div class="stock-product__name">
+                            {{ item.value.product_id}}
+                        </div>
+                        <input type="number"
+                               step="10"
+                               min="10"
+                               max="1000"
+                               formControlName="quantity">
+                        <button type="button">
+                            Remove
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     `,
     styleUrls: ['./stock-products.component.scss']
@@ -15,10 +31,18 @@ export class StockProductsComponent implements OnInit {
     @Input()
     parent: FormGroup;
 
+    get stocks() {
+        const sd = (this.parent.get('stock') as FormArray).controls;
+        console.warn(sd);
+        return sd;
+    }
+
+
     constructor() {
     }
 
     ngOnInit() {
+        console.warn('parent', this.parent, '5465465465');
     }
 
 }
